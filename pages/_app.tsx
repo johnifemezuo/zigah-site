bf099c2d8e9b55e1de81eba9a927dc71e184f345
johnifemezuo <ifemezuo@zigah.co>
2023-01-30 23:08:14 +0100

    installed hotjar in the website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
-import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import type { AppProps } from "next/app";
-import React from "react";
+import { useEffect } from "react";
+import { hotjar } from "react-hotjar";
+import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    hotjar.initialize(3343663, 6);
+  }, []);
+
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
